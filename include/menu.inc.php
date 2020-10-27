1292f1d83ebbf5e07fffef7d86cff4c3caa341d3
Gautier Mechaussie <49200160+Tyegoteks@users.noreply.github.com>
2020-10-27 22:59:25 +0100

    ajout des inc.php

---
+++ include/menu.inc.php
@@ -0,0 +1,11 @@
+<nav>
+  <ol>
+    <li>Acceuil</li>
+    <li>A propos</li>
+    <li>Creer un blog</li>
+    <li>Gérer son blog</li>
+    <li>Exemples de blog</li>
+    <li>Aides</li>
+    <li>Contact</li>
+  </ol>
+</nav>
